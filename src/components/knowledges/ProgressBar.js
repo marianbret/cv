4e9570da69590daa9559aac7d31db3b04b31dbb0
marian <marian.bret@epitech.eu>
2021-08-14 20:08:56 +0200

    push cv

--- src/components/knowledges/ProgressBar.js
+++ src/components/knowledges/ProgressBar.js
@@ -7,7 +7,7 @@ const ProgressBar = (props) => {
             <div className="years">
                 <span>Années d'experience</span>
                 <span>1 an</span>
-                <span>2 ans</span>
+                <span>2 ans+</span>
             </div>
             <div>
                 {
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Contact = () => {
     return (
-        <div>
+        <div className="contact">
             <Navigation />
-            contact
+            <div className="contactContent">
+                <div className="header"></div>
+                <div className="contactBox">
+                        <h1>Contactez-moi</h1>
+                        <ul>
+                            <li>
+                                <i className="fas fa-map-marker-alt"></i>
+                                <span>Lyon</span>
+                            </li>
+                            <li>
+                                <i className="fas fa-mobile-alt"></i>
+                                <CopyToClipboard text="0635448559">
+                                    <span className="clickInput" onClick={() => {alert('Téléphone copié')}}>06 35 44 85 59</span>
+                                </CopyToClipboard>
+                            </li>
+                            <li>
+                                <i className="far fa-envelope"></i>
+                                <CopyToClipboard text="marian.bret@epitech.eu">
+                                    <span className="clickInput" onClick={() => {alert('Email copié')}}>marian.bret@epitech.eu</span>
+                                </CopyToClipboard>
+                            </li>
+                        </ul>
+                    </div>
+                <div className="socialNetwork">
+                    <ul>
+                        <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <h4>LinkedIn</h4>
+                            <i className="fab fa-linkedin"></i>
+                        </a>
+                        <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <h4>Github</h4>
+                            <i className="fab fa-github"></i>
+                        </a>
+                        <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <h4>Twitter</h4>
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <h4>Codepen</h4>
+                            <i className="fab fa-codepen"></i>
+                        </a>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
